3500cd3d020ffc72e53708d88d4a543ea2bc73d4
Kamynale <47644905+Kamynale@users.noreply.github.com>
2022-05-04 18:53:07 +0300

    logs

--- src/main.js
+++ src/main.js
@@ -1,105 +1,175 @@
-const path = require('path');
-
-const { createSdk } = require('tinkoff-sdk-grpc-js');
-
-const { app } = require('./modules/server');
+const config = require('./config');
 const { logger } = require('./modules/logger');
-const { saveToken, delToken, getTokens, selectToken } = require('./modules/tokens');
 
-const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+try {
+    const { createSdk } = require('tinkoff-sdk-grpc-js');
 
-// Логи ошибок пишутся отдельно для TinkoffApi и http сервера.
-const logsServerFileName = path.join(__dirname, '../logs/server/' + new Date().toLocaleString(undefined, options) + '.txt');
+    const { app } = require('./modules/server');
+    const { tokenRequest, getSelectedToken } = require('./modules/tokens');
+    const { getFutures, getShares, getBlueChipsShares,
+        getBlueChipsFutures, getFigiData, getTradingSchedules } = require('./modules/getHeadsInstruments');
 
-// Ответ сервера, чтобы проверен что запущен.
-app.get('*/check', (req, res) => {
-    return res
-        .status(200)
-        .json({ status: true });
-});
+    const token = getSelectedToken() || config.defaultToken;
 
-app.get('*/gettokens', async (req, res) => {
-    try {
-        return res
-            .json(getTokens());
-    } catch (error) {
-        logger(logsServerFileName, error, res);
-    }
-});
+    // TODO: изменить руками или в процессе создания робота.
+    const appName = config.appName || 'pskucherov.tinkofftradingbot';
+    let sdk;
 
-app.get('*/deltoken', async (req, res) => {
-    try {
-        delToken(req.query.token);
-
-        return res
-            .json({});
-    } catch (error) {
-        logger(logsServerFileName, error, res);
-    }
-});
-
-app.get('*/selecttoken', async (req, res) => {
-    try {
-        selectToken(req.query.token);
-
-        return res
-            .json({});
-    } catch (error) {
-        logger(logsServerFileName, error, res);
-    }
-});
-
-app.get('*/addtoken', async (req, res) => {
-    let userInfo;
-    let sandboxAccounts;
-    let isSandbox = false;
-    let isProduction = false;
-
-    try {
-        const sdk = createSdk(req.query.token, 'opexviewer.addtoken');
-
-        // Сделать запрос к пользователю
-        sandboxAccounts = Boolean(await sdk.sandbox.getSandboxAccounts({}));
-
-        // Сделать запрос за пользовательской информацией.
-        userInfo = Boolean(await sdk.users.getInfo({}));
-
-        // Ошибку не обрабатываем, т.к. в данном случае это ок.
-        // В идеале в ответе иметь это сендбокс или нет.
-    } catch (e) {}
-
-    if (sandboxAccounts && userInfo) {
-        isProduction = true;
-    } else if (sandboxAccounts) {
-        isSandbox = true;
+    if (!token) {
+        logger(0, 'Нет выбранного токена. Добавьте в src/config.js руками или через opexviewer.');
     } else {
-        return res
-            .json({ error: true });
+        sdk = createSdk(token, appName);
     }
 
-    try {
-        saveToken(isSandbox, req.query.token);
-
+    // Ответ сервера, чтобы проверен что запущен.
+    app.get('*/check', (req, res) => {
         return res
-            .json({
-                token: isProduction ? 'production' : 'sandbox',
+            .status(200)
+            .json({ status: true });
+    });
+
+    // CRUD токенов.
+    tokenRequest(createSdk, app);
+
+    // Получение списка фьючерсов и акций, если их нет.
+    (async () => {
+        if (sdk) {
+            const futures = await getFutures(sdk);
+
+            futures && futures.updateDate && logger(0, 'Дата обновления списка фьючерсов: ' + futures.updateDate);
+
+            const shares = await getShares(sdk);
+
+            shares && shares.updateDate && logger(0, 'Дата обновления списка акций: ' + shares.updateDate);
+
+            const futureByFIGI = await sdk.instruments.futureBy({
+                idType: sdk.InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
+                id: 'FUTMGNT03220',
             });
-    } catch (error) {
-        logger(logsServerFileName, error, res);
+
+            const candles = await sdk.instruments.tradingSchedules({
+                exchange: 'MOEx',
+                from: new Date(),
+                to: new Date(),
+            });
+
+            /*  const candles = await sdk.marketData.getCandles({
+                 figi: 'FUTMGNT06220',
+                 from: new Date('2022-04-25T00:00:00Z'),
+                 to: new Date('2022-04-25T24:00:00Z'),
+                 interval: sdk.CandleInterval.CANDLE_INTERVAL_5_MIN,
+             }); */
+            const fs = require('fs');
+
+            fs.writeFileSync('./mgnt.txt', JSON.stringify(candles));
+
+            console.log(JSON.stringify(candles));
+        } else {
+            logger(0, 'Укажите token для получения фьючерсов и акций.');
+        }
+    })();
+
+    app.get('/bluechipsshares', (req, res) => {
+        try {
+            return res
+                .json(getBlueChipsShares());
+        } catch (error) {
+            logger(0, error, res);
+        }
+    });
+
+    app.get('/bluechipsfutures', (req, res) => {
+        try {
+            return res
+                .json(getBlueChipsFutures(sdk));
+        } catch (error) {
+            logger(0, error, res);
+        }
+    });
+
+    app.get('/figi/:figi', (req, res) => {
+        const figi = req.params.figi;
+
+        try {
+            const data = getFigiData(figi);
+
+            if (!data) {
+                return res.status(404).end();
+            }
+
+            return res
+                .json(data);
+        } catch (error) {
+            logger(0, error, res);
+        }
+    });
+
+    app.get('/tradingschedules', async (req, res) => {
+        try {
+            const data = await getTradingSchedules(sdk, req.query.exchange);
+
+            if (!data) {
+                return res.status(404).end();
+            }
+
+            return res.json(data);
+        } catch (error) {
+            logger(0, error, res);
+        }
+    });
+
+    app.get('/getcandles/:figi', async (req, res) => {
+        const figi = req.params.figi;
+
+        try {
+            const candles = await sdk.marketData.getCandles({
+                figi: 'FUTMGNT06220',
+                from: new Date('2022-04-25T00:00:00Z'),
+                to: new Date('2022-04-25T24:00:00Z'),
+                interval: sdk.CandleInterval.CANDLE_INTERVAL_5_MIN,
+            });
+            const fs = require('fs');
+
+            fs.writeFileSync('./mgnt.txt', JSON.stringify(candles));
+
+            return res
+                .json(getBlueChipsFutures(sdk));
+        } catch (error) {
+            logger(0, error, res);
+        }
+    });
+
+    app.get('/logs', async (req, res) => {
+        const path = require('path');
+
+        const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+        const dateStr = new Date().toLocaleString(undefined, options);
+
+        const fs = require('fs');
+        
+        const dataPath = path.join(__dirname, `../logs/server/${dateStr}.txt`);
+
+        fs.readFile(dataPath, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(data);
+        });
+    });
+
+    app.get('/', async (req, res) => {
+        try {
+            return res
+                .json(await checkToken(sdk));
+        } catch (error) {
+            logger(0, error, res);
+        }
+    });
+
+    function checkToken(sdk) {
+        return sdk.users.getAccounts();
     }
-});
-
-app.get('/', async (req, res) => {
-    try {
-        const sdk = createSdk(req.query.token, req.query.appname);
-
-        return res
-            .json(await checkToken(sdk));
-    } catch (error) {
-        logger(logsServerFileName, error, res);
-    }
-});
-
-function checkToken(sdk) {
-    return sdk.users.getAccounts();
+} catch (error) {
+    logger(0, error);
 }
